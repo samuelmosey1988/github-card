3091cdde9312288982a1c0e8598242575fd3fb3e
samuelmosey1988 <samuelmosey1988@gmail.com>
2018-08-07 11:48:02 -0400

    second part of assessment

--- src/App.js
+++ src/App.js
@@ -3,12 +3,19 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state={users: null, active: false}
   handleClick=() => {
-    console.log("party commenced")
+    fetch("https://api.github.com/users/samuelmosey1988")
+    .then(res => res.json())
+    .then(data => {
+      this.setState({users: data, active: true})
+      //this.setState triggers re-render automatically
+    })
   }
   render() {
     return <button onClick={this.handleClick}> party button </button> 
   }
 }
+
 
 export default App;
